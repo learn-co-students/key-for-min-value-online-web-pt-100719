8e38bfe5948b102a98be5033c80d2548a1cdb543
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-10-14 17:23:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
 
+  # declare variables as placeholders
+
+  key = nil # placeholder for name
+  min_value = nil # placeholder for minimum numeric value
+  
+  # Use minimum value to display respective key name
+
+  name_hash.each do |name, num|
+    if num < 2
+      key = name
+    elsif num < 17
+      key = name
+    else
+      nil
+    end
+  end
+ 
+  key
+
 end
