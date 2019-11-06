d21c3820b833251784217d4796cf1d8a33e3f6ce
Henry Allan <henryallan50@gmail.com>
2019-11-06 21:08:29 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+  smallest_yeet = ""
+  smallest_jeet = 999999999999999999999
+
+  name_hash.each do |yeet, jeet|
+    
+    if jeet < smallest_jeet
+  
+      smallest_yeet = yeet
+      smallest_jeet = jeet
+  
+    end
+    
+  end
+  
+  return name_hash.key(smallest_jeet)
+  
 end
